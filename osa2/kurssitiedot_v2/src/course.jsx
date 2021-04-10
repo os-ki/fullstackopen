f6bd0b4dc681a945995cc1bcffad84fe767a90ce
Oskari Nieminen <oskari.nieminen@nitro.fi>
2021-04-10 18:29:37 +0300

    task 2.2

--- osa2/kurssitiedot_v2/src/course.jsx
+++ osa2/kurssitiedot_v2/src/course.jsx
@@ -3,10 +3,18 @@ import Header from "./header";
 import Content from "./content";
 
 const Course = ({ course }) => {
+  const getExercisesCount = () =>
+    course?.parts.reduce((a, c) => {
+      return a + c.exercises;
+    }, 0);
+
   return (
     <div className="course">
       <Header name={course?.name} />
       <Content parts={course?.parts} />
+      <p>
+        <b>Total of {getExercisesCount()} exercises</b>
+      </p>
     </div>
   );
 };
