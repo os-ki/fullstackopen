8b4290701bfaa34fadae5eba8b90101c32713e65
Oskari Nieminen <oskari.nieminen@nitro.fi>
2021-04-11 11:22:15 +0300

    task 2.8

--- osa2/puhelinluettelo/src/app.js
+++ osa2/puhelinluettelo/src/app.js
@@ -3,16 +3,18 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newTel, setNewTel] = useState("");
 
   const onFormSubmit = (e) => {
     e.preventDefault();
     if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name: newName, tel: newTel }]);
     }
 
     setNewName("");
+    setNewTel("");
   };
   return (
     <div>
@@ -28,12 +30,23 @@ const App = () => {
           />
         </div>
         <div>
+          phone:{" "}
+          <input
+            type="tel"
+            required
+            value={newTel}
+            onChange={(e) => setNewTel(e.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name}: {person.tel}
+        </p>
       ))}
     </div>
   );
